7604fb4646b90fdb1f7ca8f429376b1ca68acef9
jonathanvo <jnvo25@gmail.com>
2022-04-22 14:33:54 -0700

    Check socketId for validity

--- src/player.js
+++ src/player.js
@@ -1,5 +1,7 @@
 export default class Player {
   constructor(socketId) {
+    if (socketId.length !== 20) { throw new Error('Parameter (socketId) not a 20 char string'); }
+
     this.character = undefined;
     this.socketId = socketId;
     this.isReady = false;
