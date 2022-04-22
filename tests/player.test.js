08f09c603d3be2f911d7b799b46e96f84f3e0174
jonathanvo <jnvo25@gmail.com>
2022-04-22 14:34:03 -0700

    Add test for Player class

---
+++ tests/player.test.js
@@ -0,0 +1,20 @@
+import { describe, expect, test } from '@jest/globals';
+import Player from '../src/player';
+
+let tempPlayer;
+
+describe('Player Class', () => {
+  test('create instance', () => {
+    tempPlayer = new Player('0kjhytfds4yu7y6fdsaw');
+  });
+
+  test('reset function sets all values to original', () => {
+    tempPlayer.character = 'monkee';
+    tempPlayer.isReady = true;
+    tempPlayer.isTagged = true;
+    tempPlayer.reset();
+    expect(tempPlayer.character === 'undefined');
+    expect(tempPlayer.isReady === false);
+    expect(tempPlayer.isTagged === false);
+  });
+});
