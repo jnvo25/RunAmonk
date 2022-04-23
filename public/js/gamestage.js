f2376bc28aa7f76766151eed21cb681c5a3b902f
Jonathan Vo <jonatnv1@uci.edu>
2022-04-23 11:48:56 -0700

    Draw stage

--- public/js/gamestage.js
+++ public/js/gamestage.js
@@ -4,7 +4,24 @@ export default class GameStage extends Phaser.Scene {
     super('GameStage');
   }
 
+  preload() {
+    // Load stage assets
+    this.load.image('background', 'assets/maps/images/background.png');
+    this.load.image('spike', 'assets/maps/images/spike.png');
+    this.load.image('tiles', 'assets/maps/tilesets/terrain_tilesheet.png');
+    this.load.tilemapTiledJSON('map', 'assets/maps/tilemaps/homestage.json');
+  }
+
   create() {
+    // Create stage
+    const backgroundImage = this.add.image(0,0, 'background').setOrigin(0,0);
+    backgroundImage.setScale(2, 0.8);
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('terrain_tilesheet', 'tiles');
+    const platforms = map.createStaticLayer('Platforms', tileset, 0, 0);
+    platforms.setCollisionByExclusion(-1, true);
+
+    // Setup communications with server
     this.socket = this.registry.get('socket');
     this.setupSockets();
 
