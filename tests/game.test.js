86c805a25c9eb48baf7adf2aa44cc8429ac65baa
jonathanvo <jnvo25@gmail.com>
2022-04-23 10:40:09 -0700

    Refactor tests to be self containing

--- tests/game.test.js
+++ tests/game.test.js
@@ -1,23 +1,27 @@
 import { describe, expect, test } from '@jest/globals';
 import Game from '../public/js/game';
 
-let tempGame;
-
 describe('Game Class', () => {
   test('create instance', () => {
-    tempGame = new Game();
+    // eslint-disable-next-line no-unused-vars
+    const tempGame = new Game();
   });
 
   test('add player to game and check if in waiting room', () => {
+    const tempGame = new Game();
     tempGame.addPlayer('0kjhytfds4yu7y6fdsaw');
     expect(Array.from(tempGame.waitingPlayers)).toHaveLength(1);
   });
 
   test('get player\'s status', () => {
+    const tempGame = new Game();
+    tempGame.addPlayer('0kjhytfds4yu7y6fdsaw');
     expect(tempGame.getPlayerStatus('0kjhytfds4yu7y6fdsaw')).toBe('waiting');
   });
 
   test('delete player', () => {
+    const tempGame = new Game();
+    tempGame.addPlayer('0kjhytfds4yu7y6fdsaw');
     tempGame.deletePlayer('0kjhytfds4yu7y6fdsaw');
 
     expect(() => {
@@ -26,6 +30,7 @@ describe('Game Class', () => {
   });
 
   test('move new player from waiting to pregame status', () => {
+    const tempGame = new Game();
     tempGame.addPlayer('juh7ygt5de0fkcru7hbg');
     expect(tempGame.getPlayerStatus('juh7ygt5de0fkcru7hbg')).toBe('waiting');
 
@@ -36,19 +41,19 @@ describe('Game Class', () => {
   });
 
   test('set player as ready', () => {
+    const tempGame = new Game();
     tempGame.addPlayer('jki9okpi0ijuhyg6trfd');
     tempGame.updateReadyPlayer('jki9okpi0ijuhyg6trfd');
     expect(tempGame.players.get('waiting').get('jki9okpi0ijuhyg6trfd').isReady).toBeTruthy();
   });
 
   test('calculate if all players ready', () => {
+    const tempGame = new Game();
     tempGame.addPlayer('g6yhft5r4dswerdtguji');
+    tempGame.addPlayer('jki9okpi0ijuhyg6trfd');
+    tempGame.updateReadyPlayer('jki9okpi0ijuhyg6trfd');
     expect(tempGame.isAllReady).toBeFalsy();
     tempGame.updateReadyPlayer('g6yhft5r4dswerdtguji');
     expect(tempGame.isAllReady).toBeTruthy();
-  });
-
-  test('list all players in waiting state who are ready', () => {
-
   });
 });
