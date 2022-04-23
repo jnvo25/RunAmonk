f8dfd41e03ced10ca99c92584f5d798dbc944150
Jonathan Vo <jonatnv1@uci.edu>
2022-04-23 16:28:51 -0700

    Create player sprite

--- public/js/gamestage.js
+++ public/js/gamestage.js
@@ -5,6 +5,10 @@ export default class GameStage extends Phaser.Scene {
   }
 
   preload() {
+    // Load  Monkee assets
+    this.load.spritesheet('monkee-idle', 'assets/Monkee_Monster/Monkee_Monster_Idle_18.png', { frameWidth: 32, frameHeight: 32 });
+    this.load.spritesheet('monkee-run', 'assets/Monkee_Monster/Monkee_Monster_Run_8.png', { frameWidth: 32, frameHeight: 32 });
+
     // Load stage assets
     this.load.image('background', 'assets/maps/images/background.png');
     this.load.image('spike', 'assets/maps/images/spike.png');
@@ -18,20 +22,70 @@ export default class GameStage extends Phaser.Scene {
     backgroundImage.setScale(2, 0.8);
     const map = this.make.tilemap({ key: 'map' });
     const tileset = map.addTilesetImage('terrain_tilesheet', 'tiles');
-    const platforms = map.createStaticLayer('Platforms', tileset, 0, 0);
-    platforms.setCollisionByExclusion(-1, true);
+    this.platforms = map.createStaticLayer('Platforms', tileset, 0, 0);
+    this.platforms.setCollisionByExclusion(-1, true);
+
+    // Create animations
+    this.createAnimation('monkee-idle', 17, true);
+    this.createAnimation('monkee-run', 7, true);
 
     // Setup communications with server
     this.socket = this.registry.get('socket');
     this.setupSockets();
 
-    // TODO: Draw character
-    console.log(this.registry.get('gameRoomOccupants'));
+    // Get variables ready
+    this.registry.set('monkeeGroup', this.add.group());
+    this.registry.set('runnerGroup', this.add.group());
+
+    // Create player
+    const tempPlayerSprite = this.createPlayer(200, 200, 'monkee', 'asdfasdf');
+    
+    // console.log(this.registry.get('gameRoomOccupants'));
+  }
+
+  // TODO: Get player input
+
+  createPlayer(positionX, positionY, character, id) {
+    const tempPlayerSprite = this.physics.add.sprite(positionX, positionY, `${character}-idle`);
+
+    // Character data
+    tempPlayerSprite.anims.play(`${character}-idle`, true);
+    tempPlayerSprite.id = id;
+    tempPlayerSprite.character = character;
+    tempPlayerSprite.isTagged = false;
+    if (character === 'monkee') {
+      this.registry.get('monkeeGroup').add(tempPlayerSprite);
+    } else {
+      this.registry.get('runnerGroup').add(tempPlayerSprite);
+    }
+
+    // Character appearance
+    tempPlayerSprite.setSize(14, 27);
+    tempPlayerSprite.setOffset(8, 5);
+
+    // Character physics
+    tempPlayerSprite.setCollideWorldBounds(true);
+    this.physics.add.collider(tempPlayerSprite, this.platforms);
+
+    return tempPlayerSprite;
   }
 
   setupSockets() {
     this.socket.on('server_playerUpdate', (gameRoomOccupants) => {
       console.log('Server sent player update', gameRoomOccupants);
     });
+  }
+
+  // Add animation to phaser
+  createAnimation(name, frames, repeat) {
+    const config = {
+      key: name,
+      frames: this.anims.generateFrameNumbers(name, { start: 0, end: frames }),
+      frameRate: 10,
+    };
+    if (repeat) {
+      config.repeat = -1;
+    }
+    this.anims.create(config);
   }
 }
