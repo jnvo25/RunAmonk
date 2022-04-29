da7fba2cd1e4a5369d4c25257b195a66ed38a204
Jonathan Vo <jonatnv1@uci.edu>
2022-04-28 17:16:09 -0700

    Fix text positioning bug

--- public/js/postgamestage.js
+++ public/js/postgamestage.js
@@ -8,7 +8,7 @@ export default class PostgameStage extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(this.screenCenterX, this.registry.get('screenCenterY') - 100, `The ${'Team'} won!`, { backgroundColor: '#ffo', fontSize: '40px' }).setOrigin(0.5);
+    this.add.text(this.registry.get('screenCenterX'), this.registry.get('screenCenterY') - 100, `The ${'Team'} won!`, { backgroundColor: '#ffo', fontSize: '40px' }).setOrigin(0.5);
 
     this.playAgainButton = new Button(this.registry.get('screenCenterX'), this.registry.get('screenCenterY'), 'Play Again', this, () => {
       this.registry.get('socket').emit('client_playAgain');
